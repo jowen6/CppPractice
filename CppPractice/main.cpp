f7556334993de075ba690355f67cb3dcaa48b2ad
Justin Owen <justinowen@justins-macbook-pro.local>
2018-10-24 20:40:11 -0500

    Initial Commit

---
+++ CppPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppPractice
+//
+//  Created by Justin Owen on 10/24/18.
+//  Copyright © 2018 Justin Owen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
