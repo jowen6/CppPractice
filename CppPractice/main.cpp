7eeb531d7f607f96b694a7d61d933e50783b5cda
Justin Owen <justinowen@Justins-MacBook-Pro.local>
2018-10-24 21:31:59 -0500

    Added Euclidean norm function for C-Vector

--- CppPractice/main.cpp
+++ CppPractice/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 struct CartesianVector{
     float X;
@@ -21,15 +22,26 @@ struct CartesianVector{
 };
 
 void PrintCartesianVectorComponents(CartesianVector);
+float CartesianVectorNorm(CartesianVector);
 
 int main(int argc, const char * argv[]) {
-    CartesianVector CVec1(1,2,3);
+    CartesianVector CVec1(-3,0,0);
     CartesianVector CVec2(2.1);
+    
     PrintCartesianVectorComponents(CVec1);
     PrintCartesianVectorComponents(CVec2);
+    
+    std::cout << "Norm: "<< CartesianVectorNorm(CVec1) << "\n";
+    
     return 0;
 }
 
 void PrintCartesianVectorComponents(CartesianVector V){
     std::cout << "Components: " << V.X << ", " << V.Y << ", " << V.Z << "\n";
+}
+
+float CartesianVectorNorm(CartesianVector V){
+    float Norm = V.X*V.X + V.Y*V.Y + V.Z*V.Z;
+    Norm = std::sqrt(Norm);
+    return Norm;
 }
