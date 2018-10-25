a7cf4e1cb66a00532023c99f512127f3830c26c6
Justin Owen <justinowen@Justins-MacBook-Pro.local>
2018-10-24 22:46:03 -0500

    added overloaded +/- operators and +=/-= operator functionality for C-Vectors

--- CppPractice/main.cpp
+++ CppPractice/main.cpp
@@ -16,11 +16,15 @@ struct CartesianVector{
     float Y;
     float Z;
     
+    CartesianVector() {};
+    
     CartesianVector(float x, float y, float z) :
         X(x), Y(y), Z(z) {}
     
     CartesianVector(float x) :
         X(x), Y(x), Z(x) {}
+    
+    
     
     
     void print(){
@@ -35,19 +39,56 @@ struct CartesianVector{
     }
     
     
+    CartesianVector operator+(const CartesianVector V);
+    void operator+=(const CartesianVector V);
+    CartesianVector operator-(const CartesianVector V);
+    void operator-=(const CartesianVector V);
 };
 
 
+
+CartesianVector CartesianVector::operator+(const CartesianVector V){
+    CartesianVector VectorSum;
+    VectorSum.X = X + V.X;
+    VectorSum.Y = Y + V.Y;
+    VectorSum.Z = Z + V.Z;
+    
+    return VectorSum;
+};
+
+void CartesianVector::operator+=(const CartesianVector V){
+    CartesianVector VectorSum;
+    X = X + V.X;
+    Y = Y + V.Y;
+    Z = Z + V.Z;
+
+};
+
+CartesianVector CartesianVector::operator-(const CartesianVector V){
+    CartesianVector VectorSum;
+    VectorSum.X = X - V.X;
+    VectorSum.Y = Y - V.Y;
+    VectorSum.Z = Z - V.Z;
+    
+    return VectorSum;
+};
+
+void CartesianVector::operator-=(const CartesianVector V){
+    CartesianVector VectorSum;
+    X = X - V.X;
+    Y = Y - V.Y;
+    Z = Z - V.Z;
+    
+};
 
 
 
 int main(int argc, const char * argv[]) {
     CartesianVector CVec1(-3,0,0);
     CartesianVector CVec2(2.1);
-    
+    CVec2 -= CVec1;
     CVec1.print();
     CVec2.print();
-    
     std::cout << "Norm: "<< CVec1.norm() << "\n";
     
     return 0;
